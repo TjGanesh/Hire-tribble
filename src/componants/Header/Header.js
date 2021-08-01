f6f7671dfd40d9a88a455e01eda273ba4cf21aa0
Ganesh Sunkara <gsunkara@paypal.com>
2021-08-01 08:05:25 +0530

    intendation and select container update commit

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,18 +1,27 @@
-import {useState} from 'react';
-import './Header.css';
+import { useState } from "react";
+import "./Header.css";
 import { GoPlus } from "react-icons/go";
 
 function Landing() {
-    const [modalOpen, setModalOpen] = useState(false);
-    return (
-        <>
-            <nav className="navbar sticky-top navbar-light" style={{"backgroundColor": "#bababa",}}>
-            <div className="container-fluid">
-             <button type="button" className="btn btn-info btn-sm" onClick={()=>setModalOpen(!modalOpen)}><GoPlus></GoPlus>Add new profile</button>   
-            </div>
-            </nav>          
-        </>
-    )
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <>
+      <nav
+        className="navbar sticky-top navbar-light"
+        style={{ backgroundColor: "#bababa" }}
+      >
+        <div className="container-fluid">
+          <button
+            type="button"
+            className="btn btn-info btn-sm"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            <GoPlus></GoPlus>Add new profile
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Landing
+export default Landing;
